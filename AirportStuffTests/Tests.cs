37fd13f3ec01934778df9430499b92cc6ab5ff4e
ApisLineolata <apislineolata@gmail.com>
2020-05-19 09:09:11 -0400

    Create flight definition

--- AirportStuffTests/Tests.cs
+++ AirportStuffTests/Tests.cs
@@ -1,4 +1,5 @@
-﻿using AitportStuff;
+﻿using System;
+using AitportStuff;
 using NUnit.Framework;
 
 namespace AirportStuffTests
@@ -6,6 +7,20 @@ namespace AirportStuffTests
     [TestFixture]
     public class Tests
     {
+        [Test]
+        public void FlightInitialisedTest()
+        {
+            Location origin = new Location("YYZ", "Toronto");
+            Location destination = new Location("YUL", "Montreal");
+            DateTime departure = new DateTime(2020, 01, 01, 12, 0, 0);
+            int capacity = 20;
+            Flight flight = new Flight(origin, destination, departure, capacity);
+            Assert.That(flight.Origin, Is.EqualTo(origin));
+            Assert.That(flight.Destination, Is.EqualTo(destination));
+            Assert.That(flight.Departure, Is.EqualTo(departure));
+            Assert.That(flight.Capacity, Is.EqualTo(capacity));
+        }
+
         [Test]
         public void LocationInitialisedTest()
         {
---
+++ AitportStuff/Flight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AitportStuff
+{
+    public class Flight
+    {
+        public Flight(Location _origin, Location _destination, DateTime _departure, int _capacity)
+        {
+            Origin = _origin;
+            Destination = _destination;
+            Departure = _departure;
+            Capacity = _capacity;
+        }
+
+        public Location Origin { get; }
+        public Location Destination { get; }
+        public DateTime Departure { get; }
+        public int Capacity { get; }
+    }
+}
