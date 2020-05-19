d21eb27915c9d0e9e84a61138846b469ce320c2c
ApisLineolata <apislineolata@gmail.com>
2020-05-19 09:03:04 -0400

    Create location tests

---
+++ AirportStuffTests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using AitportStuff;
+using NUnit.Framework;
+
+namespace AirportStuffTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void LocationInitialisedTest()
+        {
+            Location location = new Location("YYZ", "Toronto");
+            Assert.That(location.Code, Is.EqualTo("YYZ"));
+            Assert.That(location.City, Is.EqualTo("Toronto"));
+        }
+    }
+}
---
+++ AitportStuff/Location.cs
@@ -0,0 +1,14 @@
+﻿namespace AitportStuff
+{
+    public class Location
+    {
+        public Location(string _code, string _city)
+        {
+            City = _city;
+            Code = _code;
+        }
+
+        public string City { get; }
+        public string Code { get; }
+    }
+}
