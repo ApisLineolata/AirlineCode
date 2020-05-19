41c5cbe3101e0cefeb0e0175cc7b8fa84dea33fe
ApisLineolata <apislineolata@gmail.com>
2020-05-19 09:29:37 -0400

    Add null schedule
    - Used when not yet scheduled

---
+++ AitportStuff/NullSchedule.cs
@@ -0,0 +1,11 @@
+﻿namespace AitportStuff
+{
+    public class NullSchedule : ISchedule
+    {
+        /// <inheritdoc />
+        public string Output()
+        {
+            return "Not scheduled";
+        }
+    }
+}
